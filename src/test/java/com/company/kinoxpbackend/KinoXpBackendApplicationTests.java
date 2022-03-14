40c554acfcc18deeecb17b0fa82f1c8f62c91a18
Jakob M <jakobmajid00@gmail.com>
2022-03-14 12:13:26 +0100

    first commit

---
+++ src/test/java/com/company/kinoxpbackend/KinoXpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.kinoxpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
